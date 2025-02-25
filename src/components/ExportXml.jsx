8814eb5e3ad184fac2e434fa1f4c2d1124c7720d
Igor <musichound.ge@gmail.com>
2025-02-25 23:07:43 +0500

    xml export added

--- src/components/ExportXml.jsx
+++ src/components/ExportXml.jsx
@@ -8,6 +8,39 @@ const ExportXml = ()=>{
     const exportList = useSelector(state => state.exportList)
     if ( exportList.length === 0) return;
 
+    const generateXML = (materials) => {
+        let xmlString = `<?xml version="1.0" encoding="UTF-8"?>\n<archive>\n  <model>\n`;
+
+        materials.forEach((material, index) => {
+            xmlString += `    <material tag="mat${index + 1}" type="Common" component="">\n`;
+            xmlString += `      <label label="${material.material_name}"/>\n`;
+            xmlString += `      <propertyGroup tag="def">\n`;
+            xmlString += `        <set name="thermalconductivity" value="${material.thermal_conductivity}"/>\n`;
+            xmlString += `        <set name="density" value="${material.density}"/>\n`;
+            xmlString += `        <set name="heatcapacity" value="${material.heat_capacity_at_constant_pressure}"/>\n`;
+            xmlString += `      </propertyGroup>\n`;
+            xmlString += `    </material>\n`;
+        });
+
+        xmlString += `  </model>\n</archive>`;
+
+        return xmlString;
+    };
+
+    const handleDownload = () => {
+        const xmlContent = generateXML(exportList);
+        const blob = new Blob([xmlContent], { type: "application/xml" });
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "data.xml";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+    };
+
     return (
             <Box
             sx={{
@@ -40,7 +73,7 @@ const ExportXml = ()=>{
                 }
                     <CustomButton
                     label={'Скачать XML'}
-                    handleClick={()=> {alert('DOWNLOADED!')}}
+                    handleClick={handleDownload}
                     />                
         </Box>
     )
